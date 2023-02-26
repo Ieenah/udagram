5d1c95f2881e05c5649c31c81793cc57907979e6
Leenah <leenahalfulaij@gmail.com>
2023-02-27 01:04:07 +0300

    final

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "udagramApi",
-  apiHost: "http://udagram-env.eba-dppryinb.us-east-1.elasticbeanstalk.com//api/v0",
+  appName: 'udagramApi',
+  apiHost: 'http://udagram-env.eba-dppryinb.us-east-1.elasticbeanstalk.com//api/v0',
 };
 
 /*
